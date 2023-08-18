dab7534dd9ec2be1fd97ca08fafc4515c3cbec8c
Tiffany Mwangi <tiffany.mwagi@student.moringaschool.com>
2023-08-18 23:32:57 +0300

    initial commit

---
+++ time.py
@@ -0,0 +1,19 @@
+def convert_to_24_hour(time_str):
+    parts = time_str.split()
+    time_parts = parts[0].split(':')
+    hour = int(time_parts[0])
+    minute = int(time_parts[1])
+    period = parts[1]
+
+    if period == "pm" and hour != 12:
+        hour += 12
+    elif period == "am" and hour == 12:
+        hour = 0
+
+    time_24_hour = f"{hour:02d}{minute:02d}"
+    return time_24_hour
+
+# Test
+time_str = "8:30 am"
+time_24_hour = convert_to_24_hour(time_str)
+print(time_24_hour)  # Output: 0830
